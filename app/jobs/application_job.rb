da9df0b24cfb2298811c7e355b50089e50ae3346
Neil Stoker <neil.stoker@gmail.com>
2019-03-17 14:11:00 +0000

    Nuke the system

--- app/jobs/application_job.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# ApplicationJob
-class ApplicationJob < ActiveJob::Base
-end
--- spec/support/factory_bot.rb
+++
@@ -1,3 +0,0 @@
-# FactoryBot.definition_file_paths.clear
-# FactoryBot.definition_file_paths << './spec/factories'
-# FactoryBot.find_definitions
