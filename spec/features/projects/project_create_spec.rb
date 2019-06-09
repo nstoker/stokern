56ed5b6a11fb0f2dd469df298a4585cb0a91906e
Neil Stoker <neil.stoker@gmail.com>
2019-06-09 21:07:14 +0100

    Improve test coverage

--- spec/features/projects/project_create_spec.rb
+++ spec/features/projects/project_create_spec.rb
@@ -7,18 +7,28 @@ feature 'New project', :devise do
   include Warden::Test::Helpers
   Warden.test_mode!
   let(:me) { create :user }
-  before { login_as me, scope: :user }
+  before do
+    login_as me, scope: :user
+    visit new_project_path
+  end
+
   after { Warden.test_reset! }
 
-  scenario 'Create via project path' do
-    visit projects_path
-    click_link 'New Project'
-    project = build :project
-    fill_in 'Name', with: project.name
-    fill_in 'Outline', with: project.outline
-    fill_in 'Notes', with: project.notes
-    select_in 'project_visibility', 'site'
-    click_button 'Create Project'
+  scenario 'good data' do
+    fill_in_details build(:project)
     expect(page).to have_content I18n.t('projects.messages.created')
   end
+
+  scenario 'Bad data' do
+    fill_in_details build(:project, :invalid)
+    expect(page).to have_content 'errors prohibited'
+  end
+end
+
+def fill_in_details(project)
+  fill_in 'Name', with: project.name
+  fill_in 'Outline', with: project.outline
+  fill_in 'Notes', with: project.notes
+  select_in 'project_visibility', 'site'
+  click_button 'Create Project'
 end
