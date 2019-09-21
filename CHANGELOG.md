b7d1f515a8d9e7d2ee80b41aba05282c25fca122
Neil Stoker <neil.stoker@gmail.com>
2019-09-21 13:52:14 +0100

    Bump 0.0.29

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 0.0.29 - 2019-09-21
+
 Dependabot alert: faker (2.4.0)
 
 ## 0.0.28 - 2019-09-19
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-APP_VERSION = '0.0.28'.freeze unless defined? APP_VERSION
+APP_VERSION = '0.0.29'.freeze unless defined? APP_VERSION
 
 module Stokern
   class Application < Rails::Application
